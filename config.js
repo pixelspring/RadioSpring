0bcb14e63d0d7670ecc1513a5d86f1f05dd5f1c6
Mike Seaby <mike@pixelspring.co.uk>
2016-12-05 15:40:16 +0000

    Update built app name

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const path = require('path')
 let config = {
   // Name of electron app
   // Will be used in production builds
-  name: 'radiospring',
+  name: 'RadioSpring',
 
   // webpack-dev-server port
   port: 9080,
